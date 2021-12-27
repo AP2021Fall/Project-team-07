777162f6534198d484a8a51d16f4d84c01d89941
amirdragon <you@example.com>
2021-12-28 00:36:04 +0330

    View Test

--- src/test/java/view/ViewTest.java
+++ src/test/java/view/ViewTest.java
@@ -1,5 +1,6 @@
 package view;
 
+import controller.JsonController;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -21,8 +22,16 @@ public class ViewTest {
         String command = "user create --username Amir --password1 2020 --password2 2020 --email Address Amir@gmail.com";
         String response = "user created successfully!";
         System.setOut(new PrintStream(outContent));
-//        System.setIn(new ByteArrayInputStream(command.getBytes()));
         View.view.register(command);
-        assertEquals(response, outContent.toString());
+        assertEquals(response, outContent.toString().trim());
+    }
+    @Test
+    public void login() throws ParseException {
+        JsonController.getInstance().readFromJson();
+        String command = "user login --username mamad --password 138014";
+        String response = "There is not any user with username: mamad!";
+        System.setOut(new PrintStream(outContent));
+        View.view.logIn(command);
+        assertEquals(response, outContent.toString().trim());
     }
 }
